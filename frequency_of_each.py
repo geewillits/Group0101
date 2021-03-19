d3d98f1da9f3aeb0ddaf71b5b63cc4a4671580e6
geewillits <64938876+geewillits@users.noreply.github.com>
2021-03-18 23:19:03 -0400

    Update frequency_of_each.py

--- frequency_of_each.py
+++ frequency_of_each.py
@@ -4,12 +4,14 @@
 import re
 #Input your text here
 stringy = input('Enter your text string here: ')
+#Use Regex to replace all non-word characters
 stringier = (re.sub(r"\W+|_", " ", stringy))
+#Convert to upper case to avoid counting differently capitalized words seperately
 str = stringier.upper()
 # break the string into list of words
 str_list = str.split()
-
+#delivers set of unique words
 unique = set(str_list)
-
+#counts each word in the unique list in the master list. boomtown
 for words in unique:
     print('Frequency of ', words, 'is :', str_list.count(words))
